e980b27bdbbecf668ec3e6ab30ba34724508ebd2
Evelyn-DeLima <fabycomy@hotmail.com>
2024-06-06 18:40:01 +0200

    Added basic form handling and Typewriter pluging

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  let recipeElement = document.querySelector("#recipe");
+
+  new Typewriter("#recipe", {
+    strings: "Brigadeiro",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
